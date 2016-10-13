49f6b28b3b1be4b614f3f5c8bd93727ef845103f
fridayy <ever2k4@gmail.com>
2016-10-13 03:54:35 +0200

    added simple logging aspect

---
+++ tethys-common/src/main/java/ninja/harmless/tethys/aop/logging/EnableLogging.java
@@ -0,0 +1,14 @@
+package ninja.harmless.tethys.aop.logging;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author bnjm@harmless.ninja - 10/13/16.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface EnableLogging {
+}
---
+++ tethys-common/src/main/java/ninja/harmless/tethys/aop/logging/LoggingAspect.java
@@ -0,0 +1,32 @@
+package ninja.harmless.tethys.aop.logging;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * This logging aspect is used to fight cross cutting concerns.
+ * Logs exceptions within methods if the method is annotated with{@link EnableLogging}
+ *
+ * @author bnjm@harmless.ninja - 10/13/16.
+ */
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("@annotation(ninja.harmless.tethys.aop.logging.EnableLogging) && execution(* * (..))")
+    public void annotationPointcut() {
+
+    }
+
+    @AfterThrowing(pointcut = "annotationPointcut()", throwing = "e")
+    public void logExceptionAfterThrowing(JoinPoint jp, Throwable e) {
+        logger.error("{} in {}.{}() thrown. Cause: {}. ", e.getMessage(), jp.getSignature().getDeclaringTypeName(), jp.getSignature().getName(), e.getCause(), e);
+    }
+}
