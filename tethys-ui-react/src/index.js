7399a79a0fad8a7e32138ebd8f79674f185d35fb
fridayy <ever2k4@gmail.com>
2016-10-13 02:05:29 +0200

    completed ReactJS initial project structure

---
+++ tethys-ui-react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './main/frontend/App'
+import './main/frontend/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
