c77d252cfe2f86fee2924cbb446b193362c55d05
fridayy <ever2k4@gmail.com>
2016-12-09 16:54:07 +0100

    changed todo from groovy to java

---
+++ tethys-backend/src/main/java/ninja/harmless/tethys/todo/model/Todo.java
@@ -0,0 +1,11 @@
+package ninja.harmless.tethys.todo.model;
+
+import ninja.harmless.tethys.data.annotation.MongoDTO;
+import ninja.harmless.tethys.data.entity.BaseEntity;
+
+/**
+ * @author bnjm@harmless.ninja - 12/9/16.
+ */
+@MongoDTO
+public class Todo extends BaseEntity {
+}
