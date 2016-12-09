c0099ce8b9e49b8499f020c8080acaa39813793a
fridayy <ever2k4@gmail.com>
2016-12-09 16:26:50 +0100

    clean up

--- tethys-backend/src/main/java/ninja/harmless/tethys/todo/model/Todo.groovy
+++ tethys-backend/src/main/java/ninja/harmless/tethys/todo/model/Todo.groovy
@@ -1,11 +1,12 @@
 package ninja.harmless.tethys.todo.model
 
 import ninja.harmless.tethys.data.annotation.MongoDTO
+import ninja.harmless.tethys.data.entity.BaseEntity
 
 /**
  * @author bnjm@harmless.ninja - 12/9/16.
  */
 @MongoDTO
-class Todo {
-    String id
+class Todo extends BaseEntity {
+
 }
