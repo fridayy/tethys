8d3e2fa924ab703382b5d1c2871c2b661e3ad6d5
fridayy <ever2k4@gmail.com>
2016-12-17 17:35:19 +0100

    added boot tests for both frontends

---
+++ tethys-ui-angular2/src/test/java/ninja/harmless/tethys/BootstrapAngular2Test.groovy
@@ -0,0 +1,19 @@
+package ninja.harmless.tethys
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import spock.lang.Specification
+/**
+ * @author bnjm@harmless.ninja - 12/17/16.
+ */
+@SpringBootTest
+class BootstrapAngular2Test extends Specification {
+    @Autowired
+    ApplicationContext ctx
+
+    void "BootstrapAngular2 provides an application context"() {
+        expect:
+            ctx != null
+    }
+}
---
+++ tethys-ui-angular2/src/test/java/ninja/harmless/tethys/controller/AngularControllerTest.groovy
@@ -0,0 +1,25 @@
+package ninja.harmless.tethys.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+/**
+ * @author bnjm@harmless.ninja - 12/17/16.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class AngularControllerTest extends Specification {
+
+    @Autowired
+    MockMvc mockMvc
+
+    void "Angular2 front-end gets provided"() {
+        expect:
+            mockMvc.perform(get("/")).andExpect(status().isOk())
+    }
+}
